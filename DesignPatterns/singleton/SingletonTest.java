5e92ac72e272956482ad5d295be6a11aeeda1614
John Wesly Kumbala <weslykumbala12@gmail.com>
2026-07-03 10:59:34 +0530

    Add files via upload

---
+++ DesignPatterns/singleton/SingletonTest.java
@@ -0,0 +1,21 @@
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        Logger logger2 = Logger.getInstance();
+
+        logger2.log("User Logged In");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+
+        System.out.println("Logger1 HashCode: " + logger1.hashCode());
+        System.out.println("Logger2 HashCode: " + logger2.hashCode());
+    }
+}
