56b6f8e79f3085903e18f19e28ea77276af21332
John Wesly Kumbala <weslykumbala12@gmail.com>
2026-07-20 14:45:40 +0530

    Add files via upload

---
+++ Spring Core Maven/Configuring the Spring IoC Container/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,21 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void service() {
+
+        System.out.println("Book Service Working...");
+
+        bookRepository.display();
+    }
+
+}
