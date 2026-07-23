a5a1453b9fd1518269c0a87c732690b3e27fa2ec
John Wesly Kumbala <weslykumbala12@gmail.com>
2026-07-23 09:25:23 +0530

    Add files via upload

---
+++ Spring Data JPA with Hibernate/Exercise1/Handson4/src/main/java/com/cognizant/spring_learn/model/Employee.java
@@ -0,0 +1,41 @@
+package com.cognizant.spring_learn.model;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ Spring Data JPA with Hibernate/Exercise1/Handson4/src/main/java/com/cognizant/spring_learn/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring_learn.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.spring_learn.model.Employee;
+import com.cognizant.spring_learn.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
