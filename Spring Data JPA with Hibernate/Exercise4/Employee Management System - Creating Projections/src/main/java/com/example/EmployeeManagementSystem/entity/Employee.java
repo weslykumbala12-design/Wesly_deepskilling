86a411e0813cbfb6a4a4d21f59732ad5b6cfa51e
John Wesly Kumbala <weslykumbala12@gmail.com>
2026-07-23 16:24:11 +0530

    Add files via upload

---
+++ Spring Data JPA with Hibernate/Exercise4/Employee Management System - Creating Projections/src/main/java/com/example/EmployeeManagementSystem/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.example.EmployeeManagementSystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+}
