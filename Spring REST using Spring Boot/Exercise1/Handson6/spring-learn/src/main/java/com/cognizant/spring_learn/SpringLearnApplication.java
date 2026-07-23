961c7bd4a71d513740aac58e9c48946c40ac6c1f
John Wesly Kumbala <weslykumbala12@gmail.com>
2026-07-23 18:02:01 +0530

    Add files via upload

---
+++ Spring REST using Spring Boot/Exercise1/Handson6/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,42 @@
+package com.cognizant.spring_learn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayCountries();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void displayCountries() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                (List<Country>) context.getBean("countryList");
+
+        LOGGER.debug("Countries : {}", countryList);
+
+        ((ClassPathXmlApplicationContext) context).close();
+
+        LOGGER.info("END");
+    }
+}
